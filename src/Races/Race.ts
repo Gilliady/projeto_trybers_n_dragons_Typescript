a15caf536c434c26d8ffc645ba84641471b3cb7f
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-09-19 18:21:09 +0000

    Requisito 01

---
+++ src/Races/Race.ts
@@ -0,0 +1,16 @@
+export default abstract class Race {
+  constructor(protected _name: string, protected _dexterity: number) { }
+  static createdRacesInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+}
