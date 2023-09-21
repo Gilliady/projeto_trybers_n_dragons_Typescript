64947a5821c7fb685333fb678ab542d62f306886
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-09-21 15:38:08 +0000

    Requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,23 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    protected player: Fighter,
+    protected opponent: Fighter,
+  ) {
+    super(player);
+  }
+
+  override fight(): number {
+    while (this.player.lifePoints > 0 && this.opponent.lifePoints > 0) {
+    //   const queue = this.player.dexterity > this.opponent.dexterity
+    //     ? [this.player, this.opponent] : [this.opponent, this.player];
+    //   queue[0].attack(queue[1]);
+    //   queue[1].attack(queue[0]);
+      this.player.attack(this.opponent);
+      this.opponent.attack(this.player);
+    }
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
