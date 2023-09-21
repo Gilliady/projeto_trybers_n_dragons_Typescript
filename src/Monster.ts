49a93f2eb0d19269b7f6efef3bbf44b599448d81
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-09-21 15:14:49 +0000

    Requisito 09

---
+++ src/Monster.ts
@@ -0,0 +1,23 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints = 85;
+  private _strength = 63;
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    return this._lifePoints;
+  }
+}
